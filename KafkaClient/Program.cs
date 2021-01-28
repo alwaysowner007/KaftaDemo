5c6601ae7f4edf2efb4ca57698b8f60eff841272
Elyor <elyor.dev@gmail.com>
2021-01-29 02:19:30 +0500

    much better than rabbit-amq

---
+++ KafkaClient/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using KafkaCommon;
+using Newtonsoft.Json;
+
+namespace KafkaClient
+{
+    internal class Program
+    {
+        private static async Task Main()
+        {
+            Console.Title = "Client";
+
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092"
+            };
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+            while (true)
+            {
+                try
+                {
+
+                    var counter = new Random().Next(1, 1000000);
+                    var item = new LotItem
+                    {
+                        Date = DateTime.UtcNow,
+                        Contract = counter,
+                        Lot = counter,
+                        Price = counter * 100m,
+                        User = $"{Guid.NewGuid()}"
+                    };
+                    var message = JsonConvert.SerializeObject(item);
+
+                    await producer.ProduceAsync("my-topic", new Message<Null, string>
+                    {
+                        Value = message
+                    });
+
+                    Console.WriteLine(message);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ KafkaServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+using KafkaCommon;
+using Newtonsoft.Json;
+
+namespace KafkaServer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Title = "Server";
+
+            var config = new ConsumerConfig
+            {
+                GroupId = "my-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using var build = new ConsumerBuilder<Ignore, string>(config).Build();
+           
+            build.Subscribe("my-topic");
+
+            while (true)
+            {
+                try
+                {
+                    var consume = build.Consume();
+                    var message = consume.Message.Value;
+                    var item = JsonConvert.DeserializeObject<LotItem>(message);
+                    var timeNow = DateTime.UtcNow;
+
+                    Console.WriteLine(timeNow.Subtract(item.Date).ToString("g"));
+                }
+                catch ( Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
